a7fe367eb3e6756ecac49df14599031dd020dce1
DFN user <dfn-user@dfntg00>
2018-11-01 11:24:55 +0800

    Added data files and testing module

---
+++ source/SDSSData/tests/test_classes.py
@@ -0,0 +1,15 @@
+
+from .spectrum import SpectralData
+
+files = ['../../../data/spec-4055-55359-0001.fits',
+         '../../../data/spec-4055-55359-0006.fits',
+         '../../../data/spec-4055-55359-0010.fits',
+         '../../../data/spec-4055-55359-0012.fits']
+
+def test_class_ra():
+    'test the SpectralData class is returning the proper ra'
+    assert # the ra is a float between 0 and 2pi
+    
+def test_class_dec():
+    'test the SpectralData class is returning the proper dec'
+    assert # the dec is a float between -pi/2 and pi/2
---
+++ source/SDSSData/tests/test_reader.py
@@ -0,0 +1,16 @@
+
+from .spectrum import spectrum_search
+
+files = ['../../../data/spec-4055-55359-0001.fits',
+         '../../../data/spec-4055-55359-0006.fits',
+         '../../../data/spec-4055-55359-0010.fits',
+         '../../../data/spec-4055-55359-0012.fits']
+
+def test_search_return():
+    'test the search returns a list of strings'
+    assert
+    
+def test_ra_dec_return():
+    'test that spectrum_search returns a list'
+    assert #the ra/dec are numbers and in the right range
+    
