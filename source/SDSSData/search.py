6af1ef5e91cd0d0431bb6dce9276e3de55d2f229
DFN user <dfn-user@dfntg00>
2018-11-01 12:44:53 +0800

    Fixed Paddys mistakes!!!

--- source/SDSSData/search.py
+++ source/SDSSData/search.py
@@ -1,12 +1,11 @@
 from SDSSData.spectrum import SpectralData 
 from astropy.io import fits
 
-filelist = list()
-
 # Takes keyword (ex. 'ra,dec,etc.') and the min/max
 # for that keyword. Returns list of all filenames
 # within the range.
 def search(filenames,keyword,min_ang,max_ang):
+    filelist = list()
     for filename in filenames:
         data = SpectralData(filename)
         if keyword == 'ra': 
