eb128dadb9fbf42a5b85c958d41fd51b7a02433d
Ronniy Joseph <ronniyjoseph@gmail.com>
2018-11-01 11:13:30 +0800

    Updated SpectralData class to have right ascension and declination methods

--- source/SDSSData/spectrum.py
+++ source/SDSSData/spectrum.py
@@ -1,11 +1,8 @@
 from astropy.io import fits
+
 import os
 
 def SpectrumSearch(path, z, kind, mag):
-
-    directory_listing = os.listdir(path)
-    for entry in directory_listing:
-
 
     return
 
@@ -13,13 +10,30 @@ def SpectrumSearch(path, z, kind, mag):
 
 class SpectralData(object):
 
-    def __init__(self):
-        self.ra = None
-        self.dec = None
+    def __init__(self, filepath=None):
+        if filepath == None:
+            raise FilePathNotSpecified("File has not been specified")
+        self.filepath = filepath
+        self.data = sfits.open(filepath)
+        self.right_ascension = None
+        self.declination = None
 
         return
 
+    @property
     def ra(self):
-        return
+        if self.right_ascension == None:
+            self.right_ascension = self.data.header["RA"]
+
+        return self.right_ascension
+
+    @property
+    def dec(self):
+        if self.declination == None:
+            self.declination = self.data.header["DEC"]
+
+        return self.declination
 
 
+class FilePathNotSpecified(Exception):
+    pass
