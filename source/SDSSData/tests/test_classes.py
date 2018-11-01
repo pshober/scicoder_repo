b80470af4be053fee1d254a4be45f9d38f09b5fc
DFN user <dfn-user@dfntg00>
2018-11-01 12:12:36 +0800

    Createdsome juicy tests.

--- source/SDSSData/tests/test_classes.py
+++ source/SDSSData/tests/test_classes.py
@@ -1,15 +1,19 @@
-
+import numpy as np
 from .SDSSData.spectrum import SpectralData
 
-files = ['../../../data/spec-4055-55359-0001.fits',
-         '../../../data/spec-4055-55359-0006.fits',
-         '../../../data/spec-4055-55359-0010.fits',
-         '../../../data/spec-4055-55359-0012.fits']
+files = ['../../data/spec-4055-55359-0001.fits',
+         '../../data/spec-4055-55359-0006.fits',
+         '../../data/spec-4055-55359-0010.fits',
+         '../../data/spec-4055-55359-0012.fits']
 
 def test_class_ra():
     'test the SpectralData class is returning the proper ra'
-    assert # the ra is a float between 0 and 2pi
+    spectral_data = SpectralData(files[0])
+    ra = spectral_data.ra
+    assert 0 < ra < 2*np.pi # the ra is a float between 0 and 2pi
     
 def test_class_dec():
     'test the SpectralData class is returning the proper dec'
-    assert # the dec is a float between -pi/2 and pi/2
+    spectral_data = SpectralData(files[0])
+    dec = spectral_data.dec
+    assert -np.pi < dec < np.pi # the dec is a float between -pi/2 and pi/2
--- source/SDSSData/tests/test_reader.py
+++ source/SDSSData/tests/test_reader.py
@@ -1,16 +1,23 @@
-
+import numpy as np
 from .SDSSData.search import search
 
-files = ['../../../data/spec-4055-55359-0001.fits',
-         '../../../data/spec-4055-55359-0006.fits',
-         '../../../data/spec-4055-55359-0010.fits',
-         '../../../data/spec-4055-55359-0012.fits']
+files = ['../../data/spec-4055-55359-0001.fits',
+         '../../data/spec-4055-55359-0006.fits',
+         '../../data/spec-4055-55359-0010.fits',
+         '../../data/spec-4055-55359-0012.fits']
 
 def test_search_return():
     'test the search returns a list of strings'
-    assert #
+    result = search(files, 'ra', 0, 2*np.pi)
+    assert isinstance(result, (list,)), 'Your search is not returning a list.'
     
-def test_ra_dec_return():
+def test_search_return2():
     'test that spectrum_search returns a list'
-    assert #the ra/dec are numbers and in the right range
+    result = search(files, 'ra', 0, 2*np.pi)
+    assert np.all([r is in files for r in result]), 'Your file is not in the given files.'
+    
+def test_search_return3():
+    'test that spectrum_search returns a list'
+    result = search(files, 'ra', -np.inf, np.inf)
+    assert len(result) == len(files), 'The search is not returning everything it is meant to.'
     
